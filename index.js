e9a7d8efcd5efa582e2761ebe6e59c128c7291f5
LCDC-LecoinDuComparateur <xcamilleimbertx@gmail.com>
2025-12-18 22:51:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export default {
     newResponse.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
     newResponse.headers.set('X-Frame-Options', 'DENY');
     newResponse.headers.set('X-Content-Type-Options', 'nosniff');
-    newResponse.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self';");
+    newResponse.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' https://js.stripe.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://*.stripe.com; font-src 'self'; connect-src 'self' https://*.stripe.com https://*.supabase.co; frame-src 'self' https://js.stripe.com https://checkout.stripe.com https://b.stripecdn.com https://newassets.hcaptcha.com;");
     
     // Important : Supprime un en-tête potentiellement conflictuel ajouté par GitHub Pages
     newResponse.headers.delete('x-github-request-id');
